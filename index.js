96151f6167985a53725645ca58dd33b6dfd058f3
Anıl Sezgin <anlsezgin3@gmail.com>
2023-09-14 23:32:14 +0300

    Added UI

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
+import express from "express";
+import bodyParser from 'body-parser';
 import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
 
-inquirer
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+{
+    res.render("index.ejs");
+});
+
+app.post('/', (req, res) =>
+{
+    const url = req.body.url;
+    if(url == null || url == undefined || url == "")
+    {
+        res.send("You must enter a URL! Go back you fool!");
+        return;
+    }
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+    });
+    let pngData = fs.readFileSync('qr_img.png');
+    res.render("qr.ejs", 
+    {
+      imageData: pngData
+    });  
+});
+/*inquirer // İf you want to use inquirer, you can use this code. But I recommend you to use ejs for user interface.
   .prompt([
     {
         message: 'Type in your URL: ',
@@ -17,7 +50,6 @@ inquirer
 
     fs.writeFile('URL.txt', url, (err) => {
         if (err) throw err;
-        console.log('The file has been saved!');
     });
   })
   .catch((error) => {
@@ -26,4 +58,9 @@ inquirer
     } else {
       // Something else went wrong
     }
-  });
+  }); */
+
+  app.listen(port, () =>
+  {
+    console.log(`Listening on port ${port}`);
+  })
